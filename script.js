4493dc355aa4e205b4446aae91565e4a739120f7
keshiagabali <150538341+keshiagabali@users.noreply.github.com>
2024-04-01 18:41:15 +0200

    Effet apparition des titres

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
 /* AFFICHAGE GENERAL */
 
+    /* Effet d’animation sur les titres */
 
+    var titres = document.querySelectorAll('.titre-anime'); // sélectionne les éléments à animer
+      
+    var observer = new IntersectionObserver(function(entries) { // détecter l'entrée des titres dans la fenêtre
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('titre-visible');
+            }
+        });
+    });
+      
+    titres.forEach(function(titre) { 
+        observer.observe(titre);
+    });
     
 });
