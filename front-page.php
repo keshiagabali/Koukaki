69205023c104479378c2a525dc3435bf26d4d3e3
keshiagabali <150538341+keshiagabali@users.noreply.github.com>
2024-04-01 20:51:17 +0200

    Section oscars

--- front-page.php
+++ front-page.php
@@ -4,16 +4,21 @@ get_header();
 ?>
 
     <main id="primary" class="site-main">
+
         <section class="banner">
             <img src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
         </section>
+
         <section id="#story" class="story">
+
             <h2>
                 <div class="titre-anime">L'histoire</div><!-- ajout de la class "titre-anime" pour animation du titre-->
             </h2>
+
             <article id="" class="story__article">
                 <p><?php echo get_theme_mod('story'); ?></p>
             </article>
+
             <?php
             $args = array(
                 'post_type' => 'characters',
@@ -24,11 +29,15 @@ get_header();
             );
             $characters_query = new WP_Query($args);
             ?>
+
             <article id="characters">
+
                 <div class="main-character">
+
                     <h3> 
                         <div class="titre-anime">Les personnages</div><!-- ajout de la class "titre-anime" pour animation du titre-->
                     </h3>
+
                     <?php
                     $main_character = $characters_query->posts[0];
                     echo '<figure>';
@@ -37,8 +46,11 @@ get_header();
                     echo '</figure>';
                     $characters_query->next_post();
                     ?>
+
                 </div>
+
                 <div class="other-characters">
+
                     <?php
                     while ( $characters_query->have_posts() ) {
                         $characters_query->the_post();
@@ -50,29 +62,46 @@ get_header();
                         echo '</figure>';
                     }
                     ?>
+
                 </div>
+
             </article>
+
             <article id="place">
+
                 <div>
+
                     <h3>
                         <div class="titre-anime">Le Lieu</div><!-- ajout de la class "titre-anime" pour animation du titre-->
                     </h3>
+
                     <p><?php echo get_theme_mod('place'); ?></p>
+
                 </div>
 
             </article>
+
         </section>
 
-
         <section id="studio">
+
             <h2> 
                 <div class="titre-anime">Studio Koukaki</div><!-- ajout de la class "titre-anime" pour animation du titre-->
             </h2>
+
             <div>
                 <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
                 <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
             </div>
-            </section>
+
+        </section>
+        
+        <section id="oscars"> <!-- ajout de la section oscars -->
+
+            <?php include ('/Users/keshia/Local Sites/koukaki/app/public/wp-content/themes/foce-child/section-oscars.php'); ?>
+
+        </section>
+
     </main><!-- #main -->
 
 <?php
---
+++ section-oscars.php
@@ -0,0 +1,5 @@
+<div class="section-oscars">
+
+    <img src="<?php echo get_stylesheet_directory_uri() . '/images/section-oscars.png' ?>" alt="image oscars" class="center-image">
+    
+</div>
